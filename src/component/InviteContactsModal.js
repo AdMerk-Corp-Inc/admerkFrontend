bcda536d47b1224f2488486d87dad42bb9899471
Arslan Jan <Arslanjs.dev@gmail.com>
2023-04-28 02:19:33 +0500

    Add Google Sign in and Fetch Contacts

--- src/component/InviteContactsModal.js
+++ src/component/InviteContactsModal.js
@@ -4,29 +4,38 @@ import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import CloseButton from "react-bootstrap/CloseButton";
-import "./InviteContactsModal.css"
+import "./InviteContactsModal.css";
+import LoadGAuth from "./LoadGAuth";
 
 function handleClose() {}
 const show = true;
 
 function InviteContactsModal() {
   return (
-      <Modal show={show} onHide={handleClose} className="" centered>
-        <Container>
-          <Row className="justify-content-between align-items-center pt-4 pb-3 px-4 border-bottom">
-            <h3 style={{width: "fit-content"}} className="p-0 m-0">Invite Contacts</h3>
-            <CloseButton className="p-0" style={{display: "none"}} />
-          </Row>
-          <Col className="p-3">
-            <p className="text-align-center m-0">Before proceeding further, please invite your contacts. You don't have to invite them manually, we will invite them for you through google. Please login with your best google account</p>
-          </Col>
-          <Modal.Footer>
-            <Button variant="primary" onClick={handleClose}>
+    <Modal show={show} onHide={handleClose} className="" centered>
+      <Container>
+        <Row className="justify-content-between align-items-center pt-4 pb-3 px-4 border-bottom">
+          <h3 style={{ width: "fit-content" }} className="p-0 m-0">
+            Invite Contacts
+          </h3>
+          <CloseButton className="p-0" style={{ display: "none" }} />
+        </Row>
+        <Col className="p-3">
+          <p className="text-align-center m-0">
+            Before proceeding further, please invite your contacts. You don't
+            have to invite them manually, we will invite them for you through
+            google. Please login with your best google account
+          </p>
+        </Col>
+        <Modal.Footer>
+          <LoadGAuth onClick={() => console.log("Fetched All Contacts, now send emails, let database know and close")}>
+            <Button variant="primary">
               Send Invite
             </Button>
-          </Modal.Footer>
-        </Container>
-      </Modal>
+          </LoadGAuth>
+        </Modal.Footer>
+      </Container>
+    </Modal>
   );
 }
 
