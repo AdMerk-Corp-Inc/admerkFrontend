ff4e6b265ab5dff80ecb73fb11aadf701651971e
Arslan Jan <Arslanjs.dev@gmail.com>
2023-04-30 02:07:08 +0500

    send emails using body rather than parameters

--- src/Helper/Helper.js
+++ src/Helper/Helper.js
@@ -8,15 +8,24 @@ export function dateTransformer(date){
     return `${date_new.getDate()}/${date_new.getMonth()+1}/${date_new.getFullYear()}`
 }
 
-export async function sendEmailInvites(emails, token) {
+export async function sendEmailInvites(emails, numbers, token) {
     console.log("[sendEmailInvites], Emails: ", emails);
     if(!emails || emails.length === 0) {
         console.log("[sendEmailInvites]: No Emails Provided");
         return ;
     }
     const emailInviteUrl = `${url}email-invite?emails=${emails.join(",")}`;
+    if(!emails) emails = [];
+    if(!numbers) numbers = [];
+    
+    const payload = {
+        emails: emails ? emails : [],
+        numbers: numbers ? numbers : []
+    }
+
     const response = await fetch(emailInviteUrl, {
-        method: 'GET',
+        method: 'POST',
+        body: JSON.stringify(payload),
         headers: {
             'Authorization': `Bearer ${token}`
         },
