b7b49c8af5fc084725589883984b35851d958f0a
Muhammad Ahmad Tahir <muhammadahmadtahir9999@gmail.com>
2023-06-16 22:01:04 +0500

    started on frontend

--- src/App.js
+++ src/App.js
@@ -6,14 +6,13 @@ import AccountProfile from "./screens/AccountProfile";
 import CommonSignup from "./screens/auth/CommonSignup";
 import SignupCustomer from "./screens/auth/SignupCustomer";
 import SignupSponser from "./screens/auth/SignupSponser";
-import Home from "./screens/Home";
 import Layout from "./screens/Layout";
 import SponsorDashboard from "./screens/sponser/SponsorDashboard";
 import RaiseTicket from "./screens/refugee/RaiseTicket";
 import RefugeeDashboard from "./screens/refugee/RefugeeDashboard";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { userContext } from "./context/UserContext";
 import Login from "./screens/auth/Login";
 import Terms from "./screens/Terms";
@@ -34,267 +33,297 @@ import JobApplicantList from "./screens/JobApplicantList";
 import VerifyEmail from "./screens/auth/VerifyEmail";
 import Loader from "./component/Loader";
 import NonAuthLayout from "./screens/NonAuthLayout";
+import SignupCompany from "./screens/auth/SignUpCompany";
+import LoginCompany from "./screens/auth/LoginCompany";
+import CompanyDashboard from "./screens/company/DashboardCompany";
 
 function App() {
-  const { user, load } = useContext(userContext);
+   const { load } = useContext(userContext);
 
-  return (
-    <>
-      <ToastContainer />
-      {load && <Loader />}
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            exact={true}
-            element={
-              <NonAuthLayout>
-                <CommonSignup />
-              </NonAuthLayout>
-            }
-          />
-          <Route
-            path="/login"
-            exact={true}
-            element={
-              <NonAuthLayout>
-                <Login />
-              </NonAuthLayout>
-            }
-          />
-          <Route
-            path="/forgot-password"
-            exact={true}
-            element={
-              <NonAuthLayout>
-                <ForgotPassword />
-              </NonAuthLayout>
-            }
-          />
-          <Route
-            path="/signup"
-            exact={true}
-            element={
-              <NonAuthLayout>
-                <CommonSignup />
-              </NonAuthLayout>
-            }
-          />
-          <Route
-            path="/signup-sponser"
-            exact={true}
-            element={
-              <NonAuthLayout>
-                <SignupSponser />
-              </NonAuthLayout>
-            }
-          />
-          <Route
-            path="/signup-refugee"
-            exact={true}
-            element={
-              <NonAuthLayout>
-                <SignupCustomer />
-              </NonAuthLayout>
-            }
-          />
-          <Route
-            path="/verifyemail"
-            exact={true}
-            element={
-              <NonAuthLayout>
-                <VerifyEmail />
-              </NonAuthLayout>
-            }
-          />
-          <Route
-            path="/terms-and-conditions"
-            exact={true}
-            element={
-              <NonAuthLayout>
-                <Terms />
-              </NonAuthLayout>
-            }
-          />
-          <Route
-            path="/privacy-policy"
-            exact={true}
-            element={
-              <NonAuthLayout>
-                <Privacy />
-              </NonAuthLayout>
-            }
-          />
+   return (
+      <>
+         <ToastContainer />
+         {load && <Loader />}
+         <BrowserRouter>
+            <Routes>
+               <Route
+                  path="/"
+                  exact={true}
+                  element={
+                     <NonAuthLayout>
+                        <CommonSignup />
+                     </NonAuthLayout>
+                  }
+               />
+               <Route
+                  path="/login"
+                  exact={true}
+                  element={
+                     <NonAuthLayout>
+                        <Login />
+                     </NonAuthLayout>
+                  }
+               />
+               <Route
+                  path="/forgot-password"
+                  exact={true}
+                  element={
+                     <NonAuthLayout>
+                        <ForgotPassword />
+                     </NonAuthLayout>
+                  }
+               />
+               <Route
+                  path="/signup"
+                  exact={true}
+                  element={
+                     <NonAuthLayout>
+                        <CommonSignup />
+                     </NonAuthLayout>
+                  }
+               />
+               <Route
+                  path="/signup-sponser"
+                  exact={true}
+                  element={
+                     <NonAuthLayout>
+                        <SignupSponser />
+                     </NonAuthLayout>
+                  }
+               />
+               <Route
+                  path="/signup-company"
+                  exact={true}
+                  element={
+                     <NonAuthLayout>
+                        <SignupCompany />
+                     </NonAuthLayout>
+                  }
+               />
+               <Route
+                  path="/login-company"
+                  exact={true}
+                  element={
+                     <NonAuthLayout>
+                        <LoginCompany />
+                     </NonAuthLayout>
+                  }
+               />
+               <Route
+                  path="/company-dashboard"
+                  exact={true}
+                  element={
+                     <Layout>
+                        <CompanyDashboard />
+                     </Layout>
+                  }
+               />
+               <Route
+                  path="/signup-refugee"
+                  exact={true}
+                  element={
+                     <NonAuthLayout>
+                        <SignupCustomer />
+                     </NonAuthLayout>
+                  }
+               />
+               <Route
+                  path="/verifyemail"
+                  exact={true}
+                  element={
+                     <NonAuthLayout>
+                        <VerifyEmail />
+                     </NonAuthLayout>
+                  }
+               />
+               <Route
+                  path="/terms-and-conditions"
+                  exact={true}
+                  element={
+                     <NonAuthLayout>
+                        <Terms />
+                     </NonAuthLayout>
+                  }
+               />
+               <Route
+                  path="/privacy-policy"
+                  exact={true}
+                  element={
+                     <NonAuthLayout>
+                        <Privacy />
+                     </NonAuthLayout>
+                  }
+               />
 
-          <Route
-            path="/change-password"
-            exact={true}
-            element={
-              <Layout>
-                <ChangePassword />
-              </Layout>
-            }
-          />
-          <Route
-            path="/create-volunteer"
-            exact={true}
-            element={
-              <Layout>
-                <Volunteer />
-              </Layout>
-            }
-          />
+               <Route
+                  path="/change-password"
+                  exact={true}
+                  element={
+                     <Layout>
+                        <ChangePassword />
+                     </Layout>
+                  }
+               />
+               <Route
+                  path="/create-volunteer"
+                  exact={true}
+                  element={
+                     <Layout>
+                        <Volunteer />
+                     </Layout>
+                  }
+               />
 
-          <Route
-            path="/sponsor-dashboard"
-            exact={true}
-            element={
-              <Layout>
-                <SponsorDashboard />
-              </Layout>
-            }
-          />
+               <Route
+                  path="/sponsor-dashboard"
+                  exact={true}
+                  element={
+                     <Layout>
+                        <SponsorDashboard />
+                     </Layout>
+                  }
+               />
 
-          <Route
-            path="/refugee-dashboard"
-            exact={true}
-            element={
-              <Layout>
-                <RefugeeDashboard />
-              </Layout>
-            }
-          />
+               <Route
+                  path="/refugee-dashboard"
+                  exact={true}
+                  element={
+                     <Layout>
+                        <RefugeeDashboard />
+                     </Layout>
+                  }
+               />
 
-          <Route
-            path="/admin-dashboard"
-            exact={true}
-            element={
-              <Layout>
-                <VolunteerAdminDashboard />
-              </Layout>
-            }
-          />
+               <Route
+                  path="/admin-dashboard"
+                  exact={true}
+                  element={
+                     <Layout>
+                        <VolunteerAdminDashboard />
+                     </Layout>
+                  }
+               />
 
-          <Route
-            path="/profile"
-            exact={true}
-            element={
-              <Layout>
-                <AccountProfile />
-              </Layout>
-            }
-          />
+               <Route
+                  path="/profile"
+                  exact={true}
+                  element={
+                     <Layout>
+                        <AccountProfile />
+                     </Layout>
+                  }
+               />
 
-          <Route
-            path="/refugee-profile"
-            exact={true}
-            element={
-              <Layout>
-                <AccountProfile />
-              </Layout>
-            }
-          />
+               <Route
+                  path="/refugee-profile"
+                  exact={true}
+                  element={
+                     <Layout>
+                        <AccountProfile />
+                     </Layout>
+                  }
+               />
 
-          <Route
-            path="/edit-profile"
-            exact={true}
-            element={
-              <Layout>
-                <EditAccountProfile />
-              </Layout>
-            }
-          />
+               <Route
+                  path="/edit-profile"
+                  exact={true}
+                  element={
+                     <Layout>
+                        <EditAccountProfile />
+                     </Layout>
+                  }
+               />
 
-          <Route
-            path="/tickets"
-            exact={true}
-            element={
-              <Layout>
-                <RaiseTicket />
-              </Layout>
-            }
-          />
+               <Route
+                  path="/tickets"
+                  exact={true}
+                  element={
+                     <Layout>
+                        <RaiseTicket />
+                     </Layout>
+                  }
+               />
 
-          <Route
-            path="/skills"
-            exact={true}
-            element={
-              <Layout>
-                <Skills />
-              </Layout>
-            }
-          />
+               <Route
+                  path="/skills"
+                  exact={true}
+                  element={
+                     <Layout>
+                        <Skills />
+                     </Layout>
+                  }
+               />
 
-          <Route
-            path="/hobby"
-            exact={true}
-            element={
-              <Layout>
-                <Hobby />
-              </Layout>
-            }
-          />
+               <Route
+                  path="/hobby"
+                  exact={true}
+                  element={
+                     <Layout>
+                        <Hobby />
+                     </Layout>
+                  }
+               />
 
-          <Route
-            path="/all-user"
-            exact={true}
-            element={
-              <Layout>
-                <AllUser />
-              </Layout>
-            }
-          />
+               <Route
+                  path="/all-user"
+                  exact={true}
+                  element={
+                     <Layout>
+                        <AllUser />
+                     </Layout>
+                  }
+               />
 
-          <Route
-            path="/all-jobs"
-            exact={true}
-            element={
-              <Layout>
-                <AllJobs />
-              </Layout>
-            }
-          />
+               <Route
+                  path="/all-jobs"
+                  exact={true}
+                  element={
+                     <Layout>
+                        <AllJobs />
+                     </Layout>
+                  }
+               />
 
-          <Route
-            path="/create-job"
-            exact={true}
-            element={
-              <Layout>
-                <CreateJob />
-              </Layout>
-            }
-          />
-          <Route
-            path="/edit-job"
-            exact={true}
-            element={
-              <Layout>
-                <EditJOb />
-              </Layout>
-            }
-          />
-          <Route
-            path="/apply-job"
-            exact={true}
-            element={
-              <Layout>
-                <JobDetailPage />
-              </Layout>
-            }
-          />
-          <Route
-            path="/job-applicant-list"
-            exact={true}
-            element={
-              <Layout>
-                <JobApplicantList />
-              </Layout>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+               <Route
+                  path="/create-job"
+                  exact={true}
+                  element={
+                     <Layout>
+                        <CreateJob />
+                     </Layout>
+                  }
+               />
+               <Route
+                  path="/edit-job"
+                  exact={true}
+                  element={
+                     <Layout>
+                        <EditJOb />
+                     </Layout>
+                  }
+               />
+               <Route
+                  path="/apply-job"
+                  exact={true}
+                  element={
+                     <Layout>
+                        <JobDetailPage />
+                     </Layout>
+                  }
+               />
+               <Route
+                  path="/job-applicant-list"
+                  exact={true}
+                  element={
+                     <Layout>
+                        <JobApplicantList />
+                     </Layout>
+                  }
+               />
+            </Routes>
+         </BrowserRouter>
+      </>
+   );
 }
 
 export default App;
--- src/screens/auth/CommonSignup.js
+++ src/screens/auth/CommonSignup.js
@@ -1,55 +1,97 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function CommonSignup() {
-    return (
-        <div className='common-signup-div container d-flex justify-content-center align-items-center'>
-            <div>
-                <div className='d-flex align-items-center justify-content-center mb-5'>
-                    <img src='/assets/images/newLogo.png' style={{ width: '19rem' }} />
-                </div>
-                <h2 className='text-center'>Join As A Company/Sponsor Or Job Seeker/Refugee</h2>
-
-                <div className='row'>
-                    <div className='col-sm-3'>
-                        <div className="col-md-12 inner-card">
-                            <img src="/assets/images/company.png" alt="" />
-                            <h3 className='text-center'>Companies Hiring</h3>
-                        </div>
-
-                        <Link className='a-btn' to="/signup-sponser?type=1">Post Free Job Now <i className="fa fa-long-arrow-right ms-3" aria-hidden="true"></i></Link>
-                    </div>
-                    <div className='col-sm-3'>
-                        <div className="col-md-12 inner-card">
-                            <img src="/assets/images/hiring.png" alt="" />
-                            <h3 className='text-center'>Sponsor</h3>
-                        </div>
-
-                        <Link className='a-btn' to="/signup-sponser?type=2">Lend A Helping Hand <i className="fa fa-long-arrow-right ms-3" aria-hidden="true"></i></Link>
-                    </div>
-
-                    <div className='col-sm-3'>
-                        <div className="col-md-12 inner-card">
-                            <img src="/assets/images/helpinghand.png" alt="" />
-                            <h3 className='text-center'>Refugee</h3>
-                        </div>
-
-                        <Link className='a-btn' to="/signup-refugee?type=1">looking For Sponsor <i className="fa fa-long-arrow-right ms-3" aria-hidden="true"></i></Link>
-                    </div>
-                    <div className='col-sm-3'>
-                        <div className="col-md-12 inner-card">
-                            <img src="/assets/images/cv.png" alt="" />
-                            <h3 className='text-center'>Job Seekers</h3>
-                        </div>
-
-                        <Link className='a-btn' to="/signup-refugee?type=2">Looking For Job<i className="fa fa-long-arrow-right ms-3" aria-hidden="true"></i></Link>
-                    </div>
-                </div>
-
-                <h6 className='font-weight-normal mt-4 text-center'>Post Resume Free! <a style={{ textDecoration: 'underline' }} href="/login">Sign in</a></h6>
+   return (
+      <div className="common-signup-div container d-flex justify-content-center align-items-center">
+         <div>
+            <div className="d-flex align-items-center justify-content-center mb-5">
+               <img
+                  src="/assets/images/newLogo.png"
+                  style={{ width: "19rem" }}
+               />
             </div>
-        </div>
-    )
+            <h2 className="text-center">
+               Join As A Company/Sponsor Or Job Seeker/Refugee
+            </h2>
+
+            <div className="row">
+               <div className="col-sm-3">
+                  <div className="col-md-12 inner-card">
+                     <img src="/assets/images/company.png" alt="" />
+                     <h3 className="text-center">Companies</h3>
+                  </div>
+
+                  {/* <Link className='a-btn' to="/signup-sponser?type=1">Post Free Job Now <i className="fa fa-long-arrow-right ms-3" aria-hidden="true"></i></Link> */}
+                  <Link className="a-btn" to="/signup-company">
+                     Sign Up{" "}
+                     <i
+                        className="fa fa-long-arrow-right ms-3"
+                        aria-hidden="true"
+                     ></i>
+                  </Link>
+                  <Link className="a-btn" to="/login-company">
+                     Login{" "}
+                     <i
+                        className="fa fa-long-arrow-right ms-3"
+                        aria-hidden="true"
+                     ></i>
+                  </Link>
+               </div>
+               <div className="col-sm-3">
+                  <div className="col-md-12 inner-card">
+                     <img src="/assets/images/hiring.png" alt="" />
+                     <h3 className="text-center">Sponsor</h3>
+                  </div>
+
+                  <Link className="a-btn" to="/signup-sponser?type=2">
+                     Lend A Helping Hand{" "}
+                     <i
+                        className="fa fa-long-arrow-right ms-3"
+                        aria-hidden="true"
+                     ></i>
+                  </Link>
+               </div>
+
+               <div className="col-sm-3">
+                  <div className="col-md-12 inner-card">
+                     <img src="/assets/images/helpinghand.png" alt="" />
+                     <h3 className="text-center">Refugee</h3>
+                  </div>
+
+                  <Link className="a-btn" to="/signup-refugee?type=1">
+                     looking For Sponsor{" "}
+                     <i
+                        className="fa fa-long-arrow-right ms-3"
+                        aria-hidden="true"
+                     ></i>
+                  </Link>
+               </div>
+               <div className="col-sm-3">
+                  <div className="col-md-12 inner-card">
+                     <img src="/assets/images/cv.png" alt="" />
+                     <h3 className="text-center">Job Seekers</h3>
+                  </div>
+
+                  <Link className="a-btn" to="/signup-refugee?type=2">
+                     Looking For Job
+                     <i
+                        className="fa fa-long-arrow-right ms-3"
+                        aria-hidden="true"
+                     ></i>
+                  </Link>
+               </div>
+            </div>
+
+            <h6 className="font-weight-normal mt-4 text-center">
+               Post Resume Free!{" "}
+               <a style={{ textDecoration: "underline" }} href="/login">
+                  Sign in
+               </a>
+            </h6>
+         </div>
+      </div>
+   );
 }
 
 export default CommonSignup;
