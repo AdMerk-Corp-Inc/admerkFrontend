52bc3dafdef019f2bc038a2e468a54cc8740fdc9
Muhammad Ahmad Tahir <muhammadahmadtahir9999@gmail.com>
2023-07-20 21:41:38 +0500

    fuck you

--- src/component/Loader.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Loader() {
-    return (
-        <div className='loader-div'>
-            <img src="/assets/images/loader1.gif" alt="" />
-        </div>
-    )
-}
